1ec38014bce1489b59c4bb32d7ccd5b8b91e4cc1
JoannaKlosinska <joanna.klosinska.55@gmail.com>
2024-02-05 12:18:20 +0100

    Improve dog_api_adapter

--- app/adapters/dog_api_adapter.rb
+++ app/adapters/dog_api_adapter.rb
@@ -4,14 +4,35 @@ class DogApiAdapter
 
   include HTTParty
   base_uri 'https://dog.ceo/api'
+
+  # Need to re-think this solution, as probably too much is happening in this adapter
   
   def self.find(name)
-    response = get("/breed/#{name.downcase}/images")
+    words = name.downcase.split(' ')
+    if words.count > 1
+      response = response_for_sub_breeds(words)
+    else
+      response = response_for_breeds(words)
+    end
+
     images = response['message']
 
     raise Errors::DogBreedNotFoundError, name unless response['status'] == 'success'
 
     random_image = images.sample
     random_image
-  end  
+  end
+
+  private
+
+  def self.response_for_breeds(words)
+    breed_name = words[0]
+    get("/breed/#{breed_name}/images")
+  end
+
+  def self.response_for_sub_breeds(words)
+    breed_name = words[1]
+    sub_breed_name = words[0]
+    get("/breed/#{breed_name}/#{sub_breed_name}/images")
+  end
 end
